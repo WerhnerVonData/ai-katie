softmax body shape fixing

katie/rl/tests/softmax_body_test.py
@@ -1,13 +1,14 @@
 import torch
+import pytest
 from katie.rl.softmax_body import SoftmaxBody
 
 
-def test_softmax_body_creation():
+def test_creation():
     sb = SoftmaxBody()
     assert (1.0 == sb.temperature)
 
 
-def test_softmax_body_call():
+def test_call():
     sb = SoftmaxBody()
     outputs = torch.tensor([[50, 20, 30, 40]])
     sb_output = sb(outputs=outputs)
@@ -16,10 +17,17 @@ def test_softmax_body_call():
     assert ((1, 3) == sb(outputs=outputs, num_samples=3).shape)
 
 
-def test_softmax_body_call_with_bigger_number_of_samples():
+def test_call_with_bigger_number_of_samples():
     sb = SoftmaxBody()
     outputs = torch.tensor([[50, 20, 30, 40]])
     sb_output = sb(outputs=outputs, num_samples=10)
     assert (torch.is_tensor(sb_output))
     print(sb_output.shape)
     assert ((1, 4) == sb_output.shape)
+
+
+def test_call_too_many_dimensions_in_outputs_shape():
+    sb = SoftmaxBody()
+    outputs = torch.tensor([[[50, 20, 30, 40]]])
+    with pytest.raises(Exception):
+        sb(outputs=outputs)